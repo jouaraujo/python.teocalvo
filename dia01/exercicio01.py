81c763d1e78d50e9892fed762b571c088f9bc02d
Jonatan <jonatansaraujo1999@gmail.com>
2024-02-07 11:55:14 -0300

    exercicios

---
+++ dia01/exercicio01.py
@@ -0,0 +1,49 @@
+"""
+Escreva um programa que receba o nome de uma pessoa
+e faça uma saudação.
+
+"Olá fulano! Seja Bem vindo!"
+"""
+
+"""
+Escreva um programa que receba o nome e a idade
+de uma pessoa. Depois exiba a mensagem:
+
+"Olá fulano, bom saber que você tem x anos.
+Seja bem vindo!"
+"""
+
+"""
+Faça um programa que receba o raio de 
+uma circunferência em centímetros. Retorne
+para o usuário qual é a área e perímetro desta
+circunferência no seguinte formato.
+
+"Área = x.xx"
+"Perímetro = y.yy"
+"""
+
+"""
+Faça um programa que receba dois valores A e B. Faça a soma
+desses dois valores e retorne o resultado:
+
+soma: x.xx
+"""
+
+"""
+Faça um programa que receba dois valores 
+A e B. Faça a potência desses dois valores e retorne o resultado
+
+a ^ b = z 
+"""
+
+"""
+Faça um programa que receba um número em segundos, converta
+ esse número para horas, minutos e segundos. Exemplos:
+ 
+ entrada : 556
+ saida : 0:9:16
+ 
+ entrada : 140153
+ saida : 38:55:53
+"""
