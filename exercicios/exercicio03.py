8c5fa55aaf6410532b7da7a8f02d3032a4273560
Jonatan <jonatansaraujo1999@gmail.com>
2024-02-12 14:17:34 -0300

    update

---
+++ exercicios/exercicio03.py
@@ -0,0 +1,66 @@
+"""
+Faça um programa que receba 4 notas de um aluno. 
+Retorne a média dessas notas, a menor e a maior nota:
+
+média: x
+menor: y
+maior: z
+"""
+
+"""
+Considere a lista: [120,"python",120.01,"asn",False,[10,20]]
+
+Faça um programa que retorne as seguintes informações:
+ -Elemento na posição -1 da lista
+ -Elemento na primeira posição da lista
+ -O último caractere do segundo elemento da lista
+ 
+Elemento -1: x
+primeiro elemento: y
+último caractere do segundo elemento: z
+"""
+
+"""
+Escreva um programa que solicite ao usuário duas
+strings e as concatene em uma única String. Em seguida,
+exiba a string resultante.
+"""
+
+"""
+Refaça o exercício 2.2 utilizando for e listas para
+receber as notas dos alunos
+"""
+
+"""
+Faça um programa com uma função que recebe
+uma frase. Para cada palavra nesta frase, inverta a 
+ordem das letras. Exiba o resultado:
+
+Esta é a frase original
+
+aesE é a esarf lanigiro 
+"""
+
+"""
+Considere a seguinte lista:
+[123,435,987,1984,2,19,423,-178,320]
+
+Faça um programa que retorne a posição do 
+menor e do maior valor encontrado:
+
+O maior valor está na posição x
+O menor valore está na posição y
+"""
+
+"""
+Escreva um programa que receba uma lista de
+números do usuário e conte quantas vezes um
+número específico aparece na lista. Solicite ao
+usuário um número e exiba a contagem.
+"""
+
+"""
+Escreva um programa que socilite ao usuário uma palavra e verifique se
+a palavra é um palíndromo (ou seja, é a mesma palavra quando lida de 
+trás para frente).
+"""
