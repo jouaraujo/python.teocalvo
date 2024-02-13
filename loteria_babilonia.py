98eb5e29831f889aacb2ed23e02745d5f8fb016b
Jonatan <jonatansaraujo1999@gmail.com>
2024-02-13 09:53:55 -0300

    dia04

--- loteria_babilonia.py
+++ loteria_babilonia.py
@@ -1,7 +1,7 @@
-try:
-    numero = int(input("Entre com um número da sorte entre 1 e 15: "))
+sorte = 7
 
-    sorte = 7
+for i in range(3):
+    numero = int(input("Entre com um número da sorte entre 1 e 15: "))
 
     if numero < sorte:
         print("Tente um número maior")
@@ -10,8 +10,9 @@ try:
         print("Tente um número menor")
 
     else:
-        print("Acertou miserávi!")
+        print("Acertou miserávi!") 
+        break
 
-except ValueError as err:
-    print("Deu merda! Tenta um número válido!")
+else:
+    print("Acabaram suas chances!")
     
