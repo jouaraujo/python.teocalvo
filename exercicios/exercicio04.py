aa2a2959309c76e5077ea73e08fa1f25afa08953
Jonatan <jonatansaraujo1999@gmail.com>
2024-02-13 12:19:41 -0300

    update

---
+++ exercicios/exercicio04.py
@@ -0,0 +1,12 @@
+"""
+Escreva um programa que crie um dicionário com nomes
+de frutas como chaves e seus respectivos preços como
+valores. Solicite ao usuário o nome de uma fruta e 
+exiba o preço correspondente.
+
+maçã: R$1,50   pera: R$1,25  goiaba: R$2,15
+
+banana: R$2,75  laranja: R$0,65  abacaxi: R$3,20
+
+Uva: R$1,90  limão: R$1,25  jaca: R$5,80
+"""
--- loteria_babilonia.py
+++ loteria_babilonia.py
@@ -1,7 +1,21 @@
+
+def entrada_usuario(msg):
+    """
+    Validação de entrada do usuário para garantir que é um número e não uma string.
+    """
+    while True:
+        try:
+            numero = int(input(msg))
+            return numero
+        except ValueError as err:
+            print("Entre com um valor válido!")
+        
+        
 sorte = 7
 
 for i in range(3):
-    numero = int(input("Entre com um número da sorte entre 1 e 15: "))
+    
+    numero = entrada_usuario("Tente um valor entre 1 e 15: ")
 
     if numero < sorte:
         print("Tente um número maior")
